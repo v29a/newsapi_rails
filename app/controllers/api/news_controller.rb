214c3a812c13fd313db3e41a41ece989cd6bb041
shashank <Shashankk165@gmail.com>
2020-08-17 01:06:12 +0530

    categorise news

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -5,7 +5,12 @@ class Api::NewsController < Api::ApplicationController
 	end
 
 	def category
-		news = News.where(category: params[:category])
-		render json: news
+		categorised_news = []
+		provider_ids = Provider.where(category: params[:category].ids
+		provider_ids.each do |provider_id|
+			feeds = News.where(provider_id: provider_id)
+			categorised_news.append(feeds)
+		end
+		render json: categorised_news
 	end
 end
