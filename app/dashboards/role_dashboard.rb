307b4253004e05c85b554d2f66e1d61ba4656304
shashank <Shashankk165@gmail.com>
2020-08-27 17:31:45 +0530

    sas

--- app/dashboards/role_dashboard.rb
+++ app/dashboards/role_dashboard.rb
@@ -23,19 +23,19 @@ class RoleDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  users
   id
   role_name
   role_description
+  users
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  users
   id
   role_name
   role_description
+  users
   created_at
   updated_at
   ].freeze
@@ -44,9 +44,9 @@ class RoleDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  users
   role_name
   role_description
+  users
   ].freeze
 
   # COLLECTION_FILTERS
